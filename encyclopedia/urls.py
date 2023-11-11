9ecd243c9917439aba79b847765852280fded64d
StanislawRodacki <rodacki.stanislaw@gmail.com>
2023-11-11 02:20:46 +0100

    Task finished. New page creating, page editing, random page button implemented

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,8 @@ app_name = "wikipedia"
 urlpatterns = [
     path("", views.index, name="index"),
     path("results", views.searchBox, name="results"),
+    path("new-page", views.newPage, name="newPage"),
+    path("edit-page", views.editPage, name="editPage"),
+    path("random-page", views.randomPage, name="randomPage"),
     path("<str:entry>", views.entryPage, name="entryPage")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import forms
-
 from . import util
-
 import markdown
+from django.contrib import messages
+import random
 
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="New entry title")
+    text = forms.CharField(label="New entry text")
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -36,5 +39,41 @@ def searchBox(request):
             return render(request, "encyclopedia/result-page.html", {"results": resultList})
     else:
         return redirect("/")
+    
+def newPage(request):
+    if request.method == "POST":
+        form = NewEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            text = form.cleaned_data["text"]
+        else:
+            return render(request, "encyclopedia/new-page.html", {"form": NewEntryForm()})
+        if title in util.list_entries():
+            return HttpResponse("Entry already exist")
+        else:
+            util.save_entry(title, text)
+            return redirect("/{}" .format(title))
+    else:
+        return render(request, "encyclopedia/new-page.html", {"form": NewEntryForm()})
         
-                
+def editPage(request):
+    if "redirect" in request.POST:
+        entry = request.POST["entry"]
+        text = util.get_entry(entry)
+        return render(request, "encyclopedia/edit-page.html", {"text":text, "entry":entry})
+    if "editText" in request.POST:
+        newText = request.POST['newText']
+        title = request.POST['title']
+        util.save_entry(title, newText)
+        return redirect("/{}" .format(title))
+    else:
+        return redirect("/")
+    
+def randomPage(request):
+    randomEntry = random.choice(util.list_entries())
+    return redirect("/{}" .format(randomEntry))
+
+
+
+
+
